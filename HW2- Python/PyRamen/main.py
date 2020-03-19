985db5820a23333844a0cfeeeb230da35e0b4527
CPix <39272924+CPix18@users.noreply.github.com>
2020-03-19 18:10:54 -0500

    3/19 change3

--- HW2- Python/PyRamen/main.py
+++ HW2- Python/PyRamen/main.py
@@ -5,3 +5,14 @@ import csv
 # Set the file path
 dir_path = Path(__file__).parents[2]
 file_path = f"{dir_path}/VU-NSH-FIN-PT-02-2020-U-C/02-Python/Homework/Instructions/PyRamen/Resources/menu_data.csv"
+
+# Initialize menu list
+menu_list = []
+
+# Open the input path as a file object
+with open(csvpath, 'r') as csvfile:
+
+    # Print the datatype of the file object
+    print(type(csvfile))
+
+    
