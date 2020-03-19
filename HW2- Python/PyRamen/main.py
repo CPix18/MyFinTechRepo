69f74d4dbe0447c72ecfaa1c4d06ddb0f0bee6bf
CPix <39272924+CPix18@users.noreply.github.com>
2020-03-19 17:42:02 -0500

    3/19 change2

--- HW2- Python/PyRamen/main.py
+++ HW2- Python/PyRamen/main.py
@@ -4,4 +4,4 @@ import csv
 
 # Set the file path
 dir_path = Path(__file__).parents[2]
-file_path = f"{dir_path}/Homework/Instructions/PyRamen/Resources/menu_data.csv"
+file_path = f"{dir_path}/VU-NSH-FIN-PT-02-2020-U-C/02-Python/Homework/Instructions/PyRamen/Resources/menu_data.csv"
