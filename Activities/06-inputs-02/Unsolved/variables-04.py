966bdc20846212bf343076b707f7470b7b32bcc9
CPix <39272924+CPix18@users.noreply.github.com>
2020-03-20 16:24:42 -0500

    3/20 change1

--- Activities/06-inputs-02/Unsolved/variables-04.py
+++ Activities/06-inputs-02/Unsolved/variables-04.py
@@ -1,24 +1,24 @@
 # Declare a variable of `name` with an input and a string of "Welcome to the Boba Shop! What is your name?".
-name = input("Welcome to the Boba Shop! What is your name?")
+
 # Check if `name` is not an empty string or equal to `None`.
-if name == "None":
+
     # If so, write a print with a string of "Hello" concatenated with the variable `name`.
-    print(name)
+   
 
     # Then, declare a variable of `beverage` with an input and a string of "What kind of boba drink would you like?".
-beverage = input("What kind of boba drink would you like?")
+
 
     # Then, Declare a variable of `sweetness` with an input and a string of "How sweet do you want your drink: 0, 50, 100, or 200?".
 
 
     # If `sweetness` equals 50 print "half sweetened".
-
+   
     # Else if `sweetness` 100 print "normal sweet".
-
+    
     # Else if `sweetness` 200 print "super sweet".
-
+   
     # Else print with a string of "non-sweet".
-
+    
 
     # Then print the string of "Your order of " concatenated with the variable `beverage`, concatenated with " boba with a sweet level of ", concatenated with the variable `sweetness`
 
@@ -27,6 +27,20 @@ beverage = input("What kind of boba drink would you like?")
 
 name = input("Welcome to the Boba Shop! What is your name?")
 beverage = input("What kind of boba drink would you like?")
+sweetness = input("How sweet do you want your drink: 0, 50, 100, or 200?")
+if name != "" or name != "None":
+    print(f"Hello {name}")
 
-if name == "None":
-     print(name)
+    if sweetness == 50:
+        sweetness = "halfsweetened"
+    if sweetness == 100:
+        sweetness = "normal sweet"
+    elif sweetness == 200:
+        sweeetness = "super sweet"
+    else:
+        sweetness = "non-sweet"
+
+    print(f"Your order of {beverage}, boba with a sweet level of {sweetness} is ready for purchase.")
+
+else:
+    print("You didn't give us your name! Goodbye")
