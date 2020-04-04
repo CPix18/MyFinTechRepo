fa620e72a05d025a3dd0c131ef5ed347749144e4
CPix <39272924+CPix18@users.noreply.github.com>
2020-04-04 15:24:18 -0500

    4/4 update3

--- 05-APIs/2/Activities/03-Stu_Under_Lock_And_Key/Solved/keys.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Set any API Keys we'll need
-export QUANDL_API_KEY="ENTER YOUR KEY HERE"
-export PLAID_CLIENT_ID="ENTER YOUR KEY HERE"
-export PLAID_PUBLIC_KEY="ENTER YOUR KEY HERE"
-export PLAID_SBX_SECRET_KEY="ENTER YOUR KEY HERE"
-
-#Run Jupyter Lab with those keys available
-jupyter lab
